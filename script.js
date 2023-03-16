e1c84342c352cbd42eb7c9c9100f3f716e7a8837
Daniel Friedrich <danielfriedrichveloso@gmail.com>
2023-03-15 21:17:42 -0300

    Adapting for Vercel

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 import * as THREE from 'three';
 import './style.css'
-import bg1 from './img/bg1.jpg'
+import bg1 from './bg1.jpg'
 
 const container = document.querySelector('.three_bg');
 const loader = new THREE.TextureLoader();
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 100);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
